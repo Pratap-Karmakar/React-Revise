49b8abb46fb0621867c55f31dd2bcb6a620ffc38
Pratap Karmakar <pratap2501.pk@gmail.com>
2024-03-12 13:08:02 +0530

    till video 5

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -9,27 +9,36 @@ function App() {
   let [counter, setCounter] = useState(0)
 
   const addValue=()=>{
-    counter = counter + 1;
+    if(counter >= 20){
+      setCounter(0)
+    }
+    else{
+      counter = counter + 1;
     setCounter(counter)
 
     // setCounter(counter +1)
 
     console.log(counter);
+    }
 
   }
 
   const removeValue = () =>{
-    counter = counter - 1;
-    setCounter(counter)
 
-    console.log(counter);
+    if(counter <= 0){
+      setCounter(0)      
+    }
+    else{
+      setCounter(counter - 1)
+      console.log(counter);
+    }
   }
 
   return (
    <div>
     <h1>Chai aur react</h1>
     <h2>Counter Value : {counter}</h2>
-    <button onClick={addValue}>Increase Value</button>
+    <button onClick={addValue} >Increase Value</button>
     <br />
     <button onClick={removeValue}>Decrease Value</button>
    </div>
